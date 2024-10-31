d55e4da421585b0d299513a19d158a1930e4dc8e
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-10-31 08:14:58 -0400

    Handle bundle annotations manually
    This is necessary now that we're migrating to
    Konflux--this was previously handled in CPAAS.
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

---
+++ build/konflux-patch.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+set -e
+
+# Gatekeeper Operator image
+stage_operator_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-15/gatekeeper-operator-3-15@sha256:registry.redhat.io/gatekeeper/gatekeeper-rhel9-operator@sha256:e8e33ff4369c91bbf63c4c81979e0231ff247f3b0ab7a2872c0918a5455a1a2d"
+operator_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9-operator@${stage_operator_img##*@}"
+# Gatekeeper image
+stage_gatekeeper_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-15/gatekeeper-3-15@sha256:fd1b924e8eb3e71dc3306067ae737b2d523ec32d7124a63c0ac1cf34e7780499"
+gatekeeper_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9@${stage_gatekeeper_img##*@}"
+
+base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
+
+csv_file=${base_dir}/bundle/manifests/gatekeeper-operator-product.clusterserviceversion.yaml
+
+# Patch images in the CSV for:
+# Gatekeeper
+#   - containerImage annotation
+#   - Deployment RELATED_IMAGE_GATEKEEPER env
+# Operator
+#   - Deployment image
+# Both
+#   - relatedImages
+csv_patch=$(printf '[{
+  "op": "replace",
+  "path": "/metadata/annotations/containerImage",
+  "value": "%s",
+},{
+  "op": "replace",
+  "path": "/spec/install/spec/deployments/0/spec/template/spec/containers/0/env",
+  "value": [{
+    "name": "RELATED_IMAGE_GATEKEEPER",
+    "value": "%s"
+  }],
+},{
+  "op": "replace",
+  "path": "/spec/relatedImages",
+  "value": [
+      { "name":"gatekeeper", "image": "%s" },
+      { "name":"gatekeeper-operator", "image": "%s" }
+  ],
+},{
+  "op": "replace",
+  "path": "/spec/install/spec/deployments/0/spec/template/spec/containers/0/image",
+  "value": "%s",
+}]' ${gatekeeper_img} ${gatekeeper_img} ${gatekeeper_img} ${operator_img} ${operator_img})
+
+kubectl patch --local=true -f ${csv_file} --type=json --patch="${csv_patch}" --output=yaml >${csv_file}.bk
+
+mv ${csv_file}.bk ${csv_file}
