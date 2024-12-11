d905920c1cd625b6383930c68a0200da867ef093
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2024-12-11 11:28:07 -0500

    Update related images/Configuration nudges
    - Add `containerImage` annotation
    - Add operator to `relatedImages`
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/konflux-patch.sh
+++ build/konflux-patch.sh
@@ -3,27 +3,46 @@
 set -e
 
 # Gatekeeper Operator image
-operator_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9-operator@sha256:6e386be134d928bdb03b702e399c97e7aedecacaa3d0813183a8c5ecf13c7bc2"
+operator_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-17/gatekeeper-3-17@sha256:0af0f40820e3d081f1dcb51edd2e89d6097f25ad9349028ca5d68dbf50abd0b8"
 # Gatekeeper image
-gatekeeper_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9@sha256:3095f68c12c5dc3b00ce84e1c37d516d96cbcb06d42eaef5372358786956bd62"
+gatekeeper_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-17/gatekeeper-operator-3-17@sha256:c6a43d63fbcc602f41df61689f8d5027b26ed4085ceb05f3ecd4e9243fbc87dd"
 
 build_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 csv_file=${build_dir}/../bundle/manifests/gatekeeper-operator.clusterserviceversion.yaml
 
+# Patch images in the CSV for:
+# Gatekeeper
+#   - containerImage annotation
+#   - Deployment RELATED_IMAGE_GATEKEEPER env
+# Operator
+#   - Deployment image
+# Both
+#   - relatedImages
 csv_patch=$(printf '[{
   "op": "replace",
-  "path": "/spec/install/spec/deployments/0/spec/template/spec/containers/0/env/0/value",
+  "path": "/metadata/annotations/containerImage",
   "value": "%s",
+},{
+  "op": "replace",
+  "path": "/spec/install/spec/deployments/0/spec/template/spec/containers/0/env",
+  "value": [{
+    "name": "RELATED_IMAGE_GATEKEEPER",
+    "value": "%s"
+  }],
+},{
+  "op": "replace",
+  "path": "/spec/relatedImages",
+  "value": [
+      { "name":"gatekeeper", "image": "%s" },
+      { "name":"gatekeeper-operator", "image": "%s" }
+  ],
 },{
   "op": "replace",
   "path": "/spec/install/spec/deployments/0/spec/template/spec/containers/0/image",
   "value": "%s",
-},{
-  "op": "replace",
-  "path": "/spec/relatedImages/0/image",
-  "value": "%s",
-}]' ${gatekeeper_img} ${operator_img} ${gatekeeper_img})
+}]' ${gatekeeper_img} ${gatekeeper_img} ${gatekeeper_img} ${operator_img} ${operator_img})
 
 kubectl patch --local=true -f ${csv_file} --type=json --patch="${csv_patch}" --output=yaml >${csv_file}.bk
+
 mv ${csv_file}.bk ${csv_file}
