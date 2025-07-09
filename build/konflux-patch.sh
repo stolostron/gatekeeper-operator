c73ac35ad9560fa674445c9af730944b59500b73
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-07-09 11:48:15 -0400

    Update gatekeeper-3-19 to b7852e6
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/konflux-patch.sh
+++ build/konflux-patch.sh
@@ -6,7 +6,7 @@ set -e
 stage_operator_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-19/gatekeeper-operator-3-19@sha256:6dd31ad9a9396b6c90f1c8d8c0af3a833c978785cdeb12cc331e37850eb3db31"
 operator_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9-operator@${stage_operator_img##*@}"
 # Gatekeeper image
-stage_gatekeeper_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-19/gatekeeper-3-19@sha256:dd27144a5cf91f04205dc4198200d63d0a6c23d5c21960cf7314139bcdbbaf78"
+stage_gatekeeper_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-19/gatekeeper-3-19@sha256:b7852e616b6f9e6a70b2fe13668cc03a487729f67ce9d148079a3c072e763c0b"
 gatekeeper_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9@${stage_gatekeeper_img##*@}"
 
 base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
