44168b14f5e80f3ba80e015793b5fd70e93503c1
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2025-10-16 14:26:05 -0400

    Discard unused files; Explicitly disable metrics
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- main.go
+++ main.go
@@ -62,11 +62,13 @@ func init() {
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
+	var (
+		enableLeaderElection bool
+		probeAddr            string
+		metricsAddr          string
+	)
 
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
