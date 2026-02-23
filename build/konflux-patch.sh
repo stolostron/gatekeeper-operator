d10d4ab0207e8ad0636a8b0f8ae8513ef6f7caa7
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-02-23 15:35:22 -0500

    Red Hat Konflux update gatekeeper-operator-3-21
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- build/konflux-patch.sh
+++ build/konflux-patch.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Gatekeeper Operator image
-stage_operator_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-20/gatekeeper-operator-3-20@sha256:c8a24039115a2a9e1674301378a4bb46c10ae44da32cb6348f68c818a25d5bcd"
+stage_operator_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-21/gatekeeper-operator-3-21@sha256:c8a24039115a2a9e1674301378a4bb46c10ae44da32cb6348f68c818a25d5bcd"
 operator_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9-operator@${stage_operator_img##*@}"
 # Gatekeeper image
-stage_gatekeeper_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-20/gatekeeper-3-20@sha256:dec7f30b7ab5509b7f1751446065e3a46e90fdea88560a2f6644bed459ac0ae2"
+stage_gatekeeper_img="quay.io/redhat-user-workloads/gatekeeper-tenant/gatekeeper-operator-3-21/gatekeeper-3-21@sha256:dec7f30b7ab5509b7f1751446065e3a46e90fdea88560a2f6644bed459ac0ae2"
 gatekeeper_img="registry.redhat.io/gatekeeper/gatekeeper-rhel9@${stage_gatekeeper_img##*@}"
 
 base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
